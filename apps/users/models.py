234ac39b2a62cc400e486d47b3663d6424210786
Luke <lukeroge@gmail.com>
2019-10-31 12:34:58 +1300

    More code quality fixes.

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin, AbstractUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
 
 class User(AbstractUser):
     username = models.CharField(
