e94d66197985fa258adc5b35b8b81b94767deb60
Luke <lukeroge@gmail.com>
2019-10-31 12:31:45 +1300

    Remove un-needed imports.

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,6 +1,5 @@
 # users/admin.py
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from .models import User, Profile
