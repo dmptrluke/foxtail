21d11c5c87c4b131d94028960decad60f8761a37
Luke <lukeroge@gmail.com>
2019-11-07 18:30:23 +1300

    Fix some config issues and standardise things.

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,12 +1,13 @@
 from django.views.generic import TemplateView
 from mozilla_django_oidc.views import OIDCLogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class UserView(TemplateView):
+class UserView(LoginRequiredMixin, TemplateView):
     template_name = "profile.html"
 
 
-class LogoutView(OIDCLogoutView):
+class LogoutView(LoginRequiredMixin, OIDCLogoutView):
     def get(self, request):
         return self.post(request)
 
