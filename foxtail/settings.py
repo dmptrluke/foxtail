11a1c285024d68affbe5ddda786670e11b832d10
Luke <lukeroge@gmail.com>
2019-11-11 23:52:08 +1300

    Make website name properly dynamic.

--- foxtail/settings.py
+++ foxtail/settings.py
@@ -81,6 +81,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.sites.middleware.CurrentSiteMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'oidc_provider.middleware.SessionManagementMiddleware',
     'csp.middleware.CSPMiddleware'
