b4d226b37770ab0558172d66d8dccd153a1ebf31
Luke <lukeroge@gmail.com>
2019-11-14 13:02:27 +1300

    Improve user admin listing a little.

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -4,4 +4,12 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'name', 'date_joined', 'is_active']
+
+    def name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
+
+
+admin.site.register(User, CustomUserAdmin)
