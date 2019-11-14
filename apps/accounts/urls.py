9574e2cb558a9d6bfe46dd6f4b318f91e7153f43
Luke <lukeroge@gmail.com>
2019-11-14 13:11:50 +1300

    Add noopener/noreferrer to all external links, remove unused import

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 
 from apps.accounts import views
 
 urlpatterns = [
-
     path('profile', views.UserView.as_view(), name='account_profile')
 ]
