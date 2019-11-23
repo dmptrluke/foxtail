f8fbf877e7337c9cee3c9a07d2bc606aad0bb64c
Luke Rogers <lukeroge@gmail.com>
2019-11-24 01:52:35 +1300

    Add comment deletion.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,15 @@
 import 'bootstrap';
 import 'svgxuse';
 
-const maxLength = 280;
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
 
     $('#id_text').keyup(function () {
         let length = $(this).val().length;
-        length = maxLength - length;
+        let limit = $(this).attr('maxlength');
+
+        length = limit - length;
         let plural = (length === 1) ? "" : "s";
         $('#hint_id_text').text(length + ' character' + plural + ' remaining.');
     });
