77815a39b7d6a3aad2ea6188dc4155133eb6fd4d
Luke Rogers <lukeroge@gmail.com>
2019-11-23 23:12:02 +1300

    Add character count limits to comments.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,15 @@
 import 'bootstrap';
 import 'svgxuse';
 
+const maxLength = 280;
+
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $('#id_text').keyup(function () {
+        let length = $(this).val().length;
+        length = maxLength - length;
+        let plural = (length === 1) ? "" : "s";
+        $('#hint_id_text').text(length + ' character' + plural + ' remaining.');
+    });
 });
