0be015c84554870a300b39970bb3decd1e819e11
Luke Rogers <lukeroge@gmail.com>
2019-11-24 16:36:52 +1300

    Increase admin form field sizes.

--- apps/content/admin.py
+++ apps/content/admin.py
@@ -1,6 +1,8 @@
 from adminsortable2.admin import SortableAdminMixin
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
+from django.db.models import TextField
+from django.forms import Textarea
 
 from .models import Page
 
@@ -10,6 +12,10 @@ class PageAdmin(SortableAdminMixin, ModelAdmin):  # lgtm [py/conflicting-attribu
         (None, {'fields': ('title', 'subtitle', 'body')}),
         ('Advanced options', {'fields': ('slug', 'show_in_menu'),}),
     )
+
+    formfield_overrides = {
+        TextField: {'widget': Textarea(attrs={'rows': 40, 'cols': 120})},
+    }
 
     list_display = ('title', 'slug', 'modified', 'show_in_menu')
     list_editable = ['show_in_menu']
