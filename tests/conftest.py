d0c2e624287dd9c5fd670489aff0567371469a0e
Luke Rogers <luke@dmptr.com>
2019-11-27 19:04:25 +1300

    Attempt to get Chrome webdriver from the enviroment for Github Actions
    Signed-off-by: Luke Rogers <luke@dmptr.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from django.core.management import call_command
 from selenium import webdriver
 
@@ -12,11 +13,15 @@ def django_db_setup(django_db_setup, django_db_blocker):
 @pytest.fixture(scope='module')
 def driver(request):
     """Provide a selenium webdriver instance."""
-    # SetUp
+    driver = os.environ.get('ChromeWebDriver', None)
+
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
 
-    browser = webdriver.Chrome(options=options)
+    if driver:
+        browser = webdriver.Chrome(driver, options=options)
+    else:
+        browser = webdriver.Chrome(options=options)
 
     yield browser
 
