747421d3282b59fa1b0fe354ad855a846b7250a4
Luke Rogers <luke@dmptr.com>
2019-12-04 19:46:41 +1300

    Slightly nicer logging config.
    Signed-off-by: Luke Rogers <luke@dmptr.com>

--- foxtail/settings.py
+++ foxtail/settings.py
@@ -407,8 +407,7 @@ LOGGING = {
     "disable_existing_loggers": False,
     "formatters": {
         "verbose": {
-            "format": "%(levelname)s %(asctime)s %(module)s "
-            "%(process)d %(thread)d %(message)s"
+            "format": "[%(asctime)s] [%(process)d] [%(levelname)s] (%(module)s) %(message)s"
         }
     },
     "handlers": {
