2c7dc90962100c9b945bd1a1224624fb737bfaab
Luke Rogers <luke@dmptr.com>
2019-12-06 17:24:38 +1300

    Add a few bits to help with testing later.
    Signed-off-by: Luke Rogers <luke@dmptr.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from django.contrib.auth import get_user_model
 from django.core.management import call_command
 from selenium import webdriver
 
@@ -7,6 +8,17 @@ from selenium import webdriver
 def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
         call_command('loaddata', 'tests/data.json')
+
+
+@pytest.fixture()
+def user(db):
+    """Add a test user to the database."""
+    user_ = get_user_model().objects.create(
+        username='test',
+        email='test@example.com'
+    )
+
+    return user_
 
 
 @pytest.fixture(scope='module')
@@ -22,3 +34,21 @@ def driver(request):
 
     # TearDown
     browser.quit()
+
+
+@pytest.fixture()
+def authenticated_driver(driver, client, live_server, user, settings):
+    """Return a browser instance with logged-in user session."""
+    settings.CSRF_COOKIE_SECURE = False
+    settings.SESSION_COOKIE_SECURE = False
+    settings.CSRF_COOKIE_NAME = 'csrftoken'
+    settings.SESSION_COOKIE_NAME = 'sessionid'
+
+    client.force_login(user)
+    cookie = client.cookies[settings.SESSION_COOKIE_NAME]
+
+    driver.get(live_server.url)
+    driver.add_cookie({'name': settings.SESSION_COOKIE_NAME, 'value': cookie.value, 'secure': False, 'path': '/'})
+    driver.refresh()
+
+    return driver
