f7b21c44bf2cf5bddc9c0688f41632ba849ea8cd
Luke Rogers <luke@dmptr.com>
2019-12-08 23:35:53 +1300

    Improve tag ordering.
    Signed-off-by: Luke Rogers <luke@dmptr.com>

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -5,7 +5,11 @@ from .models import Event
 
 
 class EventAdmin(ModelAdmin):
-    list_display = ('title', 'start')
+    list_display = ('title', 'start', 'tag_list')
+
+    @staticmethod
+    def tag_list(obj):
+        return ", ".join(o.name for o in obj.tags.all().order_by('name'))
 
 
 admin.site.register(Event, EventAdmin)
