7e5eeee113ac99452ae36d8f2b8a1f4253da827d
Luke Rogers <luke@dmptr.com>
2019-12-13 12:35:47 +1300

    Add RSS link to blog, tidy up SVG icons
    Signed-off-by: Luke Rogers <luke@dmptr.com>

--- apps/core/templatetags/icon.py
+++ apps/core/templatetags/icon.py
@@ -11,30 +11,30 @@ register = template.Library()
 
 
 @register.simple_tag(name='icon')
-def icon_tag(icon_id):
+def icon_tag(icon_id, tag='div'):
     # abuse the django slugify function to clean file names
     icon_id = slugify(icon_id)
 
     # see if we've already handled and loaded this icon before
-    cached_icon = cache.get(f'icon_{icon_id}')
+    cached_icon = cache.get(f'icon3_{icon_id}')
 
     if cached_icon:
-        return mark_safe(cached_icon)
+        icon_data = cached_icon
+    else:
+        # no cached icon, damn. now I have to work with files
+        icon_path = finders.find(f'icons/{icon_id}.svg')
 
-    # no cached icon, damn. now I have to work with files
-    icon_path = finders.find(f'icons/{icon_id}.svg')
+        if not os.path.isfile(icon_path):
+            # yeet
+            raise FileNotFoundError
 
-    if not os.path.isfile(icon_path):
-        # yeet
-        raise FileNotFoundError
+        with open(icon_path) as icon_file:
+            icon_data = icon_file.read()
+            cache.set(f'icon3_{icon_id}', icon_data, None)
 
-    with open(icon_path) as icon_file:
-        html = format_html("""
-        <div class='icon icon-{0} svg-baseline'>
-            {1}
-        </div>
-        """, icon_id, mark_safe(icon_file.read()))
-
-        cache.set(f'icon_{icon_id}', html, None)
-
+    html = format_html("""
+        <{0} class='icon icon-{1} svg-baseline'>
+            {2}
+        </{0}>
+        """, tag, icon_id, mark_safe(icon_data))
     return html
