452f86216a42d5764ac3e2b90c2f5637d55bbf03
Luke Rogers <luke@dmptr.com>
2019-12-13 19:38:35 +1300

    Fix FURRYNZ-34
    Signed-off-by: Luke Rogers <luke@dmptr.com>

--- apps/content/views.py
+++ apps/content/views.py
@@ -15,7 +15,7 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         today = datetime.today()
         context['post_list'] = Post.objects.all()[:3]
-        context['event_list'] = Event.objects.filter(end__date__gte=today)[:3]
+        context['event_list'] = Event.objects.filter(end__gte=today)[:3]
         return context
 
 
