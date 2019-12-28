b04674ba8dc68fc496661cdfae736432b3c590eb
Luke Rogers <luke@dmptr.com>
2019-12-28 18:59:18 +1300

    Check for document readiness using vanilla JS.
    Signed-off-by: Luke Rogers <luke@dmptr.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ import 'regenerator-runtime/runtime';
 import 'bootstrap';
 import 'colcade';
 
-$(function () {
+function on_load() {
     $('[data-toggle="tooltip"]').tooltip();
 
     $('#id_text').keyup(function () {
@@ -15,4 +15,10 @@ $(function () {
         let plural = (length === 1) ? "" : "s";
         $('#hint_id_text').text(length + ' character' + plural + ' remaining.');
     });
-});
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', on_load);
+} else {
+    on_load();
+}
