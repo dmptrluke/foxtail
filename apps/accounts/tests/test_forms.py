0019d43a9571b1da2ef4dcc5e77b45ff8bdcc2b9
Luke Rogers <luke@dmptr.com>
2020-01-03 16:32:11 +1300

    Expand SignupForm tests.
    Signed-off-by: Luke Rogers <luke@dmptr.com>

--- apps/accounts/tests/test_forms.py
+++ apps/accounts/tests/test_forms.py
@@ -1,9 +1,11 @@
 import pytest
+from faker import Faker
 
 from apps.accounts.forms import SignupForm
 
 from .factories import UserFactory
 
+fake = Faker()
 pytestmark = pytest.mark.django_db
 
 
@@ -17,13 +19,92 @@ class TestSignupForm:
                 "email": proto_user.email,
                 "password1": proto_user._password,
                 "password2": proto_user._password,
-                "captcha": "N/A"
             }
         )
 
         assert form.is_valid()
 
-    def test_invalid_form(self):
+    def test_duplicate_username(self, user):
+        proto_user = UserFactory.build()
+
+        form = SignupForm(
+            {
+                "username": user.username,
+                "email": proto_user.email,
+                "password1": proto_user._password,
+                "password2": proto_user._password,
+            }
+        )
+
+        assert not form.is_valid()
+        assert len(form.errors) == 1
+        assert "username" in form.errors
+
+    def test_duplicate_email(self, user):
+        proto_user = UserFactory.build()
+
+        form = SignupForm(
+            {
+                "username": proto_user.username,
+                "email": user.email,
+                "password1": proto_user._password,
+                "password2": proto_user._password,
+            }
+        )
+
+        assert not form.is_valid()
+        assert len(form.errors) == 1
+        assert "email" in form.errors
+
+    def test_invalid_email(self):
+        proto_user = UserFactory.build()
+
+        form = SignupForm(
+            {
+                "username": proto_user.username,
+                "email": 'chocolate',
+                "password1": proto_user._password,
+                "password2": proto_user._password,
+            }
+        )
+
+        assert not form.is_valid()
+        assert len(form.errors) == 1
+        assert "email" in form.errors
+
+    def test_missing_email(self):
+        proto_user = UserFactory.build()
+
+        form = SignupForm(
+            {
+                "username": proto_user.username,
+                "email": "",
+                "password1": proto_user._password,
+                "password2": proto_user._password,
+            }
+        )
+
+        assert not form.is_valid()
+        assert len(form.errors) == 1
+        assert "email" in form.errors
+
+    def test_poor_password(self):
+        proto_user = UserFactory.build()
+
+        form = SignupForm(
+            {
+                "username": proto_user.username,
+                "email": proto_user.email,
+                "password1": 'password',
+                "password2": 'password',
+            }
+        )
+
+        assert not form.is_valid()
+        assert len(form.errors) == 1
+        assert "password1" in form.errors
+
+    def test_mismatched_password(self):
         proto_user = UserFactory.build()
 
         form = SignupForm(
@@ -31,9 +112,10 @@ class TestSignupForm:
                 "username": proto_user.username,
                 "email": proto_user.email,
                 "password1": proto_user._password,
-                "password2": 'not at all the correct password',
-                "captcha": "N/A"
+                "password2": fake.password(),
             }
         )
 
         assert not form.is_valid()
+        assert len(form.errors) == 1
+        assert "password2" in form.errors
