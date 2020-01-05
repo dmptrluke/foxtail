1a9950476752f55144905bd452d61c73b11f7b82
Luke Rogers <luke@dmptr.com>
2020-01-05 15:03:14 +1300

    Start squashing accounts migrations.
    Signed-off-by: Luke Rogers <luke@dmptr.com>

---
+++ apps/accounts/migrations/0001_squashed_0008_remove_user_gender.py
@@ -0,0 +1,48 @@
+import django.contrib.auth.models
+import django.utils.timezone
+from django.db import migrations, models
+
+import apps.accounts.validators
+
+
+class Migration(migrations.Migration):
+
+    replaces = [('accounts', '0001_initial_squashed'), ('accounts', '0004_add_full_name'), ('accounts', '0005_merge_names'), ('accounts', '0006_username_spaces'), ('accounts', '0007_remove_user_profile_url'), ('accounts', '0008_remove_user_gender')]
+
+    initial = True
+
+    dependencies = [
+        ('auth', '0011_update_proxy_permissions'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
+                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
+                ('username', models.CharField(error_messages={'unique': 'A user with that username already exists.'}, help_text='Required. 30 characters or fewer. Letters, digits, spaces, and @/./+/-/_ only.', max_length=30, unique=True, validators=[apps.accounts.validators.UsernameValidator()], verbose_name='username')),
+                ('first_name', models.CharField(blank=True, max_length=30, verbose_name='first name')),
+                ('last_name', models.CharField(blank=True, max_length=150, verbose_name='last name')),
+                ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
+                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
+                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
+                ('email', models.EmailField(max_length=254, verbose_name='email address')),
+                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.Group', verbose_name='groups')),
+                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.Permission', verbose_name='user permissions')),
+                ('date_of_birth', models.DateField(blank=True, null=True)),
+                ('full_name', models.CharField(blank=True, max_length=120)),
+            ],
+            options={
+                'verbose_name': 'user',
+                'verbose_name_plural': 'users',
+                'abstract': False,
+            },
+            managers=[
+                ('objects', django.contrib.auth.models.UserManager()),
+            ],
+        )
+
+    ]
