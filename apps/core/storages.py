9aae68abc8c2965f3a9f2339414f82008b06c677
Luke Rogers <luke@dmptr.com>
2020-01-22 19:13:08 +1300

    Move some files around, add new page titles for improved SEO.
    Signed-off-by: Luke Rogers <luke@dmptr.com>

--- apps/core/storages.py
+++ apps/core/storages.py
@@ -1,11 +1,13 @@
 from storages.backends.azure_storage import AzureStorage
 
 
+# noinspection PyAbstractClass
 class StaticAzureStorage(AzureStorage):
     azure_container = 'static'
     cache_control = 'public,max-age=31536000,immutable'
 
 
+# noinspection PyAbstractClass
 class MediaAzureStorage(AzureStorage):
     azure_container = 'media'
     cache_control = 'public,max-age=604800,immutable'
