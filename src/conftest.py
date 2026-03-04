d134e38f49914ae204edc97de4bfad7d6d33a42d
Luke Rogers <luke@dmptr.com>
2026-03-05 08:21:29 +1300

    fix: increase headless chrome viewport size

--- src/conftest.py
+++ src/conftest.py
@@ -44,6 +44,7 @@ def driver(request):
     options.add_argument('--headless=new')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1920,1080')
     browser = webdriver.Chrome(options=options)
 
     yield browser
