50ec6818603c765d39bbc1e3341155d7d4bba136
Luke Rogers <luke@dmptr.com>
2026-03-05 18:04:39 +1300

    feat: new account dashboard

--- src/apps/accounts/views.py
+++ src/apps/accounts/views.py
@@ -3,11 +3,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
 from django.views import View
-from django.views.generic import ListView, UpdateView
+from django.views.generic import ListView, TemplateView, UpdateView
 
+from allauth.account.models import EmailAddress
 from allauth.account.views import PasswordResetView, SignupView
 from allauth.mfa.base.views import AuthenticateView
 from allauth.mfa.models import Authenticator
+from allauth.socialaccount.models import SocialAccount
 from csp_helpers.mixins import CSPViewMixin
 from oidc_provider.models import Token, UserConsent
 
@@ -32,8 +34,38 @@ class CSPPasswordResetView(CSPViewMixin, PasswordResetView):
     pass
 
 
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = 'account/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        user = self.request.user
+
+        emails = EmailAddress.objects.filter(user=user)
+        ctx['primary_email'] = emails.filter(primary=True).first()
+        ctx['email_count'] = emails.count()
+
+        authenticator_types = set(
+            Authenticator.objects.filter(user=user)
+            .exclude(type=Authenticator.Type.RECOVERY_CODES)
+            .values_list('type', flat=True)
+        )
+        ctx['mfa_enabled'] = len(authenticator_types) > 0
+        ctx['mfa_methods'] = authenticator_types
+
+        ctx['app_count'] = UserConsent.objects.filter(user=user).count()
+
+        social_accounts = SocialAccount.objects.filter(user=user)
+        ctx['social_count'] = social_accounts.count()
+        ctx['social_providers'] = list(social_accounts.values_list('provider', flat=True))
+
+        ctx['has_password'] = user.has_usable_password()
+
+        return ctx
+
+
 class UserView(LoginRequiredMixin, UpdateView):
-    template_name = 'account/account.html'
+    template_name = 'account/settings.html'
     form_class = UserForm
 
     def get_object(self, queryset=None):
@@ -44,7 +76,7 @@ class UserView(LoginRequiredMixin, UpdateView):
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse('account_profile')
+        return reverse('account_settings')
 
 
 class ConsentList(LoginRequiredMixin, ListView):
@@ -65,4 +97,4 @@ class ConsentRevoke(LoginRequiredMixin, View):
         return redirect('account_application_list')
 
 
-__all__ = ['UserView', 'ConsentList', 'ConsentRevoke']
+__all__ = ['DashboardView', 'UserView', 'ConsentList', 'ConsentRevoke']
