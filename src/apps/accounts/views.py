c9e3cb345b66dfeafdd6b8217280c28af5ecd586
Luke Rogers <luke@dmptr.com>
2026-03-05 15:03:42 +1300

    refactor: standardise, tidy, and document account pages

--- src/apps/accounts/views.py
+++ src/apps/accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from django.views.generic import ListView, UpdateView
@@ -35,6 +36,10 @@ class UserView(LoginRequiredMixin, UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+    def form_valid(self, form):
+        messages.success(self.request, "Your account details have been updated.")
+        return super().form_valid(form)
 
     def get_success_url(self):
         return reverse('account_profile')
