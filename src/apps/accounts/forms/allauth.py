18200da7684deded3f1f75f960988e01b5acce91
Luke Rogers <luke@dmptr.com>
2026-03-08 20:41:38 +1300

    POST-based social login buttons, condensed password help text

--- src/apps/accounts/forms/allauth.py
+++ src/apps/accounts/forms/allauth.py
@@ -17,6 +17,7 @@ class SignupForm(CSPFormMixin, auth_forms.SignupForm):
         super().__init__(*args, **kwargs)
         self.fields['username'].help_text = 'Required. 150 characters or fewer.'
         self.fields['email'].help_text = 'Required. This must be a valid email address for account activation.'
+        self.fields['password1'].help_text = "At least 8 characters. Can't be all numbers or a common password."
         if settings.RECAPTCHA_ENABLED:
             self.fields['captcha'].label = False
 
