ce727292c4a6e5da7abe746f35bcb8ec75e4a9f6
Luke Rogers <luke@dmptr.com>
2026-03-08 16:17:46 +1300

    refactor: overhaul icon system - replace SVGs, eliminate wrapper, use inline-block

--- src/apps/core/templatetags/icon.py
+++ src/apps/core/templatetags/icon.py
@@ -1,7 +1,6 @@
 from django import template
 from django.contrib.staticfiles import finders
 from django.core.cache import cache
-from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 from django.utils.text import slugify
 
@@ -9,11 +8,11 @@ register = template.Library()
 
 
 @register.simple_tag(name='icon')
-def icon_tag(icon_id, tag='div', size=None, colored=False):
+def icon_tag(icon_id, size=None, colored=False):
     # abuse the django slugify function to clean file names
     icon_id = slugify(icon_id)
     prefix = 'colored/' if colored else ''
-    cache_key = f'icon4_{prefix}{icon_id}'
+    cache_key = f'icon5_{prefix}{icon_id}'
 
     # see if we've already handled and loaded this icon before
     cached_icon = cache.get(cache_key)
@@ -28,20 +27,12 @@ def icon_tag(icon_id, tag='div', size=None, colored=False):
 
         with open(icon_path) as icon_file:
             icon_data = icon_file.read()
-
             cache.set(cache_key, icon_data, None)
 
+    attrs = f'class="icon icon-{icon_id}"'
     if size:
-        # Inject inline style on the SVG to override SCSS dimensions
-        icon_data = icon_data.replace('<svg ', f'<svg style="width:{size};height:{size}" ', 1)
+        attrs += f' style="width:{size};height:{size}"'
 
-    return format_html(
-        """
-        <{0} class='icon icon-{1} svg-baseline'>
-            {2}
-        </{0}>
-        """,
-        tag,
-        icon_id,
-        mark_safe(icon_data),  # noqa: S308
-    )
+    icon_data = icon_data.replace('<svg ', f'<svg {attrs} ', 1)
+
+    return mark_safe(icon_data)  # noqa: S308
