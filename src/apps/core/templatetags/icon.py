a36682e93179f81d630346d5155a20dcb525bbb5
Luke Rogers <luke@dmptr.com>
2026-03-11 16:53:56 +1300

    migrate icons to cotton components, rewrite icon tag with render_component

--- src/apps/core/templatetags/icon.py
+++ src/apps/core/templatetags/icon.py
@@ -1,38 +1,21 @@
 from django import template
-from django.contrib.staticfiles import finders
-from django.core.cache import cache
 from django.utils.safestring import mark_safe
 from django.utils.text import slugify
+
+from django_cotton import render_component
 
 register = template.Library()
 
 
-@register.simple_tag(name='icon')
-def icon_tag(icon_id, size=None, colored=False):
-    # abuse the django slugify function to clean file names
+@register.simple_tag(takes_context=True, name='icon')
+def icon_tag(context, icon_id, size=None, colored=False):
     icon_id = slugify(icon_id)
-    prefix = 'colored/' if colored else ''
-    cache_key = f'icon5_{prefix}{icon_id}'
+    prefix = 'colored.' if colored else ''
+    component_name = f'icons.{prefix}{icon_id}'
 
-    # see if we've already handled and loaded this icon before
-    cached_icon = cache.get(cache_key)
-
-    if cached_icon:
-        icon_data = cached_icon
-    else:
-        # no cached icon, damn. now I have to work with files
-        icon_path = finders.find(f'icons/{prefix}{icon_id}.svg')
-        if not icon_path:
-            return ''
-
-        with open(icon_path) as icon_file:
-            icon_data = icon_file.read()
-            cache.set(cache_key, icon_data, None)
-
-    attrs = f'class="icon icon-{icon_id}"'
+    kwargs = {}
     if size:
-        attrs += f' width="{size}" height="{size}"'
+        kwargs['width'] = size
+        kwargs['height'] = size
 
-    icon_data = icon_data.replace('<svg ', f'<svg {attrs} ', 1)
-
-    return mark_safe(icon_data)  # noqa: S308
+    return mark_safe(render_component(context['request'], component_name, **kwargs))  # noqa: S308
