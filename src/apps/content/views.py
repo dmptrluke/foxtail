926e4feb76fb8c9fd11f73e171db3cb5c857844f
Luke Rogers <luke@dmptr.com>
2026-03-17 21:16:13 +1300

    add authenticated dashboard cards, image perf attrs, fix avatar overflow and |safe XSS

--- src/apps/content/views.py
+++ src/apps/content/views.py
@@ -7,7 +7,7 @@ from published.utils import queryset_filter
 from structured_data.views import StructuredDataMixin
 
 from apps.blog.models import Post
-from apps.events.models import Event
+from apps.events.models import Event, EventInterest
 from apps.organisations.models import Organisation
 
 from .models import Page
@@ -32,7 +32,33 @@ class IndexView(StructuredDataMixin, TemplateView):
         if next_event:
             context['days_until'] = (next_event.start - today).days
 
+        if getattr(self.request, 'user', None) and self.request.user.is_authenticated:
+            self._add_dashboard_context(context, today)
+
         return context
+
+    def _add_dashboard_context(self, context, today):
+        from allauth.mfa.models import Authenticator
+
+        user = self.request.user
+
+        user_events = (
+            EventInterest.objects.filter(user=user)
+            .filter(Q(event__start__gte=today) | Q(event__end__gte=today))
+            .select_related('event')
+            .order_by('event__start')
+        )
+        context['user_event_count'] = user_events.count()
+        first_interest = user_events.first()
+        if first_interest:
+            context['user_next_event'] = first_interest.event
+            context['user_next_event_days'] = (first_interest.event.start - today).days
+
+        context['org_count'] = Organisation.objects.count()
+
+        has_mfa = Authenticator.objects.filter(user=user).exclude(type=Authenticator.Type.RECOVERY_CODES).exists()
+        account_age = (now() - user.date_joined).days
+        context['show_mfa_nudge'] = not has_mfa and account_age < 30
 
     def get_structured_data(self):
         return {
