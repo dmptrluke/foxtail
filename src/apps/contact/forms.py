94a5cb0f62184044f99be26b410783e59db20bad
Luke Rogers <luke@dmptr.com>
2026-04-10 00:56:23 +1200

    no turnstile for signed-in users. remove hasher we never used

--- src/apps/contact/forms.py
+++ src/apps/contact/forms.py
@@ -8,3 +8,11 @@ class ContactForm(GuardedFormMixin, CSPFormMixin, Form):
     name = CharField(required=True)
     email = EmailField(required=True)
     message = CharField(required=True, widget=Textarea)
+
+    def __init__(self, *args, **kwargs):
+        from formguard.contrib.turnstile.checks import TurnstileCheck
+
+        super().__init__(*args, **kwargs)
+        # skip Turnstile for authenticated users
+        if self.request and self.request.user.is_authenticated:
+            self._checks = [c for c in self._checks if not isinstance(c, TurnstileCheck)]
