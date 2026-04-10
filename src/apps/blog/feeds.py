15180e2cc00687c18fc5170374e9b284633854f7
Luke Rogers <luke@dmptr.com>
2026-04-10 17:12:12 +1200

    fix sticky headers, switch to atom feed

--- src/apps/blog/feeds.py
+++ src/apps/blog/feeds.py
@@ -1,29 +1,108 @@
 import html
+import logging
 
+from django.conf import settings
 from django.contrib.syndication.views import Feed
+from django.templatetags.static import static
+from django.urls import reverse_lazy
+from django.utils.feedgenerator import Atom1Feed, Enclosure
 from django.utils.html import strip_tags
+from django.utils.text import Truncator
 
 from published.utils import queryset_filter
 
+from apps.core.models import SiteSettings
+
 from .models import Post
+
+logger = logging.getLogger(__name__)
+
+
+class FoxtailAtomFeed(Atom1Feed):
+    """Atom feed with full HTML content and webfeeds namespace."""
+
+    def root_attributes(self):
+        attrs = super().root_attributes()
+        attrs['xmlns:webfeeds'] = 'http://webfeeds.org/rss/1.0'
+        return attrs
+
+    def add_root_elements(self, handler):
+        super().add_root_elements(handler)
+        if self.feed.get('webfeeds_logo'):
+            handler.addQuickElement('webfeeds:logo', self.feed['webfeeds_logo'])
+        if self.feed.get('webfeeds_accent_color'):
+            handler.addQuickElement('webfeeds:accentColor', self.feed['webfeeds_accent_color'])
+        handler.addQuickElement('webfeeds:related', '', {'layout': 'card', 'target': 'browser'})
+
+    def add_item_elements(self, handler, item):
+        super().add_item_elements(handler, item)
+        if item.get('content_html'):
+            handler.addQuickElement('content', item['content_html'], {'type': 'html'})
 
 
 class LatestEntriesFeed(Feed):
+    feed_type = FoxtailAtomFeed
     title = 'Latest News'
-    link = '/blog/'
-    description = 'The latest furry news.'
+    link = reverse_lazy('blog:list')
+    subtitle = 'The latest furry news.'
+
+    def feed_url(self):
+        return reverse_lazy('blog:feed')
+
+    def feed_extra_kwargs(self, obj):
+        logo_url = static('images/paw.svg')
+        if not logo_url.startswith('http'):
+            logo_url = settings.SITE_URL + logo_url
+        conf = SiteSettings.get_solo()
+        return {
+            'webfeeds_logo': logo_url,
+            'webfeeds_accent_color': conf.theme_color.lstrip('#'),
+        }
 
     def items(self):
-        return queryset_filter(Post.objects).order_by('-created')[:20]
+        return queryset_filter(Post.objects).select_related('author').prefetch_related('tags').order_by('-created')[:20]
 
     def item_title(self, item):
         return item.title
 
     def item_description(self, item):
-        return html.unescape(strip_tags(item.text_rendered))
+        """Short summary for the Atom <summary> element."""
+        if item.description:
+            return item.description
+        return Truncator(html.unescape(strip_tags(item.text_rendered))).chars(200)
 
     def item_pubdate(self, item):
         return item.created
+
+    def item_updateddate(self, item):
+        return item.modified
+
+    def item_author_name(self, item):
+        if item.author:
+            return item.author.name
+        return None
+
+    def item_categories(self, item):
+        return [tag.name for tag in item.tags.all()]
+
+    def item_enclosures(self, item):
+        if not item.image:
+            return []
+        try:
+            url = item.image.card_2x
+            if not url.startswith('http'):
+                url = settings.SITE_URL + url
+            return [self._enclosure(url)]
+        except Exception:
+            logger.exception('Missing image rendition for post %s', item.pk)
+            return []
+
+    @staticmethod
+    def _enclosure(url):
+        return Enclosure(url=url, length='0', mime_type='image/webp')
+
+    def item_extra_kwargs(self, item):
+        return {'content_html': item.text_rendered}
 
 
 __all__ = ['LatestEntriesFeed']
--- src/apps/blog/tests/test_feed.py
+++ src/apps/blog/tests/test_feed.py
@@ -22,6 +22,11 @@ class TestLatestEntriesFeed:
         assert feed.feed.subtitle == 'The latest furry news.'
         assert urlparse(feed.feed.link).path == reverse('blog:list')
 
+    # content type is Atom XML
+    def test_content_type(self, client, post: Post):
+        response = client.get(self.url)
+        assert 'application/atom+xml' in response['Content-Type']
+
     # only published posts appear in feed, ordered by date descending
     def test_published_posts_only(self, client, post: Post, second_post: Post, hidden_post: Post):
         post.created = datetime(2019, 12, 2, tzinfo=UTC)
@@ -43,3 +48,52 @@ class TestLatestEntriesFeed:
         assert urlparse(entry.link).path == post.get_absolute_url()
         expected = post.created.replace(microsecond=0)
         assert datetime(*entry.published_parsed[:6], tzinfo=UTC) == expected
+
+    # summary uses the post's description field when set
+    def test_item_summary_from_field(self, client, post: Post):
+        post.description = 'A short summary.'
+        post.save()
+        response = client.get(self.url)
+        feed = feedparser.parse(response.content)
+        assert feed.entries[0].summary == 'A short summary.'
+
+    # summary falls back to truncated body when description field is empty
+    def test_item_summary_fallback(self, client, post: Post):
+        post.description = ''
+        post.save()
+        response = client.get(self.url)
+        feed = feedparser.parse(response.content)
+        assert len(feed.entries[0].summary) <= 203  # 200 chars + ellipsis
+
+    # full HTML content in <content> element
+    def test_content_html(self, client, post: Post):
+        response = client.get(self.url)
+        feed = feedparser.parse(response.content)
+        entry = feed.entries[0]
+        assert entry.get('content')
+        assert post.text_rendered.strip() in entry.content[0].value
+
+    # author name included when post has an author
+    def test_item_author(self, client, post: Post):
+        response = client.get(self.url)
+        feed = feedparser.parse(response.content)
+        assert feed.entries[0].get('author') == post.author.name
+
+    # tags mapped to Atom categories
+    def test_item_categories(self, client, post: Post):
+        post.tags.add('testing', 'rss')
+        response = client.get(self.url)
+        feed = feedparser.parse(response.content)
+        tags = {t.term for t in feed.entries[0].tags}
+        assert 'testing' in tags
+        assert 'rss' in tags
+
+    # webfeeds namespace declared in feed XML
+    def test_webfeeds_namespace(self, client, post: Post):
+        response = client.get(self.url)
+        assert b'xmlns:webfeeds=' in response.content
+
+    # webfeeds accent color present
+    def test_webfeeds_accent_color(self, client, post: Post):
+        response = client.get(self.url)
+        assert b'<webfeeds:accentColor>281e33</webfeeds:accentColor>' in response.content
